92ac94f2139c16bd303ab0937e1b457cd9db9f59
Valdecio Vinicius Plakitquen <valvplakitquen@hotmail.com>
2016-03-27 21:26:25 -0300

    estou tentando XD, mas esta com errinhos

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -2,7 +2,11 @@ package ch.makery.address;
 
 import java.io.IOException;
 
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -13,6 +17,32 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+    
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    /**
+     * Construtor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    /**
+     * Retorna os dados como uma observable list de Persons. 
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -46,19 +76,6 @@ public class MainApp extends Application {
     /**
      * Mostra o person overview dentro do root layout.
      */
-    public void showPersonOverview() {
-        try {
-            // Carrega o person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
-            AnchorPane personOverview = (AnchorPane) loader.load();
-
-            // Define o person overview dentro do root layout.
-            rootLayout.setCenter(personOverview);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Retorna o palco principal.
@@ -67,6 +84,28 @@ public class MainApp extends Application {
     public Stage getPrimaryStage() {
         return primaryStage;
     }
+    
+    public void showPersonOverview() {
+    	public void showPersonOverview() {
+    	    try {
+    	        // Carrega a person overview.
+    	        FXMLLoader loader = new FXMLLoader();
+    	        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+    	        AnchorPane personOverview = (AnchorPane) loader.load();
+
+    	        // Define a person overview no centro do root layout.
+    	        rootLayout.setCenter(personOverview);
+
+    	        // Dá ao controlador acesso à the main app.
+    	        PersonOverviewController controller = loader.getController();
+    	        controller.setMainApp(this);
+
+    	    } catch (IOException e) {
+    	        e.printStackTrace();
+    	    }
+    	}
+    }
+
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -0,0 +1,104 @@
+package ch.makery.address.view;
+
+import ch.makery.address.MainApp;
+import ch.makery.address.model.Person;
+import ch.makery.address.util.DateUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    // Reference to the main application.
+    private MainApp mainApp;
+
+    /**
+     * O construtor.
+     * O construtor é chamado antes do método inicialize().
+     */
+    public PersonOverviewController() {
+    }
+
+    /**
+     * Inicializa a classe controller. Este método é chamado automaticamente
+     *  após o arquivo fxml ter sido carregado.
+     */
+    @FXML
+    private void initialize() {
+        // Inicializa a tabela de pessoas com duas colunas.
+        firstNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().lastNameProperty());
+
+        // Limpa os detalhes da pessoa.
+        showPersonDetails(null);
+
+        // Detecta mudanças de seleção e mostra os detalhes da pessoa quando houver mudança.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+
+    /**
+     * É chamado pela aplicação principal para dar uma referência de volta a si mesmo.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Adiciona os dados da observable list na tabela
+        personTable.setItems(mainApp.getPersonData());
+    }
+    
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            // Preenche as labels com informações do objeto person.
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+
+            // TODO: Nós precisamos de uma maneira de converter o aniversário em um String! 
+            // birthdayLabel.setText(...);
+        } else {
+            // Person é null, remove todo o texto.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+        }
+    }
+    
+   
+    
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        personTable.getItems().remove(selectedIndex);
+    }
+    
+}
